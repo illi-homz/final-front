b1079f674d62e4d625b76f5bd0efaaea428eb18f
illi-homz <gopkabel@mail.ru>
2020-07-07 18:37:34 +0300

    initial deploy

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
+var history = require("connect-history-api-fallback");
 
-const app = express()
+const app = express();
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+let https_redirect = function(req, res, next) {
+  if (process.env.NODE_ENV === "production") {
+    if (req.headers["x-forwarded-proto"] != "https") {
+      return res.redirect("https://" + req.headers.host + req.url);
+    } else {
+      return next();
+    }
+  } else {
+    return next();
+  }
+};
 
-const port = process.envPORT || 8080
-app.listen(port)
+app.use(https_redirect);
+app.use(history());
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
 
-console.log('Listening on port: ' + port)
+const port = process.env.PORT || 8080;
+app.listen(port);
+
+console.log('Listening on port: ' + port);
